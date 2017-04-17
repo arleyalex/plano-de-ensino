0d5c2117afa8d4765c73a32e269f26618e414e82
Arley Alex <arleyalexelias@hotmail.com>
2017-04-17 10:29:21 -0300

    Delete sqlCurso.sql

--- sqlCurso.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE curso (
-	numero INT PRIMARY KEY NOT NULL,
-	nome VARCHAR (150) NOT NULL,
-	sigla CHAR(3) NOT NULL,
-	tipo CHAR(1) NOT NULL,
-	matricula INT FOREIGN KEY (matricula) REFERENCES (professor)
-	)
