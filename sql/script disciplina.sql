45488bcfd050e5384cb0ac9c1e6e5fd7b006501b
samily <samilygois@gmail.com>
2017-05-06 15:30:25 -0300

    scriipt do banco de dados da tela disciplina

---
+++ sql/script disciplina.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE disciplina
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+CREATE TABLE curso (
+	codigo INT PRIMARY KEY NOT NULL,
+	nome VARCHAR (150) NOT NULL,
+	ch int(60) NOT NULL,
+	ementa VARCHAR(600) NOT NULL,
+	objetivo VARCHAR (600) NOT NULL,
+	bibliografia_basica VARCHAR (200) NOT NULL,
+	bibliografia_complementar VARCHAR (200) NOT NULL,
+	)
+	
