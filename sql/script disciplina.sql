6a9b21a53ce6b2fbde8a731f0187a08f3fd4e214
samily <samilygois@gmail.com>
2017-05-06 15:36:14 -0300

    script disciplina

--- sql/script disciplina.sql
+++ sql/script disciplina.sql
@@ -9,7 +9,7 @@
 CREATE TABLE curso (
 	codigo INT PRIMARY KEY NOT NULL,
 	nome VARCHAR (150) NOT NULL,
-	ch int(60) NOT NULL,
+	ch INT(60) NOT NULL,
 	ementa VARCHAR(600) NOT NULL,
 	objetivo VARCHAR (600) NOT NULL,
 	bibliografia_basica VARCHAR (200) NOT NULL,
