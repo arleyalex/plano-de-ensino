96eb3b223a8426b0269a244c2dd043a25565ab59
samily <samilygois@gmail.com>
2017-05-11 20:52:25 -0300

    script da tabela disciplina

--- sql/script disciplina.sql
+++ sql/script disciplina.sql
@@ -1,12 +1,4 @@
-﻿CREATE DATABASE disciplina
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'Portuguese_Brazil.1252'
-       LC_CTYPE = 'Portuguese_Brazil.1252'
-       CONNECTION LIMIT = -1;
-
-CREATE TABLE curso (
+﻿CREATE TABLE curso (
 	codigo INT PRIMARY KEY NOT NULL,
 	nome VARCHAR (150) NOT NULL,
 	ch INT(60) NOT NULL,
